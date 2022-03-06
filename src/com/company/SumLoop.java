90380299247cf6f83291f89559966f5d832937d3
sanjanenic <sanjanenicbl@gmail.com>
2022-03-06 22:27:40 +0100

    summ with for, correct

--- src/com/company/SumLoop.java
+++ src/com/company/SumLoop.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class SumLoop {
+    public static void main(String[] args) {
+int sum=0;
+for(int i =0; i<=100; i++){
+
+    sum = sum + i;
+
 }
+ System.out.println(sum);
+    }
+
+}
+//Add up all numbers between 1 and 100 and then print the total sum to the console. Solve the task directly in the main method.
