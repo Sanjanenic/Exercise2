d13d09eca6aad35089134ce0a4fa67128ba25a6c
sanjanenic <sanjanenicbl@gmail.com>
2022-03-06 22:09:25 +0100

    Correct

---
+++ src/com/company/Trangle.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Trangle {
+    public static void main(String[] args) {
+        for (int i =0; i<10; i++){ //definise kiliko puta ce raditi
+            for(int j=0; j<=i; j++){ //
+
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
