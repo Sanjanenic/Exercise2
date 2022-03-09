a0f9d4bfb9a0340e2facaa1fa3ae47c581137b4d
sanjanenic <sanjanenicbl@gmail.com>
2022-03-09 14:56:17 +0100

    Lösung von Chan

--- src/com/company/ArraySum.java
+++ src/com/company/ArraySum.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 
 public class ArraySum {
     public static void main(String[] args) {
-        int[]numbers = new int []{10,20,30,};
+        int[]numbers = new int []{10,20,30};
+
         calculateSum(numbers);
  //Scanner sc =new Scanner(System.in);
         //System.out.println("Enter the size of array!");
        // int size= sc.nextInt();
        // int array= new
 
-
-
     }
-
     public static int calculateSum(int[] numbers){
    int sum=0;
 
    for (int i =0; i<numbers.length; i++){
        sum += numbers[i];
-
    }
         System.out.println(sum);
    return sum;
-
 
     }
 }
--- src/com/company/PerfectNumbers.java
+++ src/com/company/PerfectNumbers.java
@@ -2,8 +2,22 @@ package com.company;
 
 public class PerfectNumbers {
     public static void main(String[] args) {
+     perfectNumber(110);
 
+    }
+    public static void perfectNumber(int numberRange){
+        for (int number=1; number <= numberRange; number++){
+            int sum = 0;
+            for(int i =1; i<=number/2; i++){
+                if(number%i ==0){
+                    sum= sum+ i;
+                }
+            }
+            if(sum ==number){
 
+                System.out.println(number +" is a perfect number!");
+            }
+        }
     }
 
 }
